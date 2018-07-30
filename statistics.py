6f65b030995ad7216c5a33cfe29e4b0a1bab40d6
LordMufasa <pfbleimer@gmail.com>
2018-07-29 21:55:44 -0400

    Began adding log mean function in statistics

--- statistics.py
+++ statistics.py
@@ -1,32 +1,36 @@
-# Statistics module watches for changes in the Ledger it is given and maintains an updated 
+# Statistics module watches for changes in the Ledger it is given and maintains an updated
 # playbook of all of the statistics regard the information in that ledger
 #
 # The API allows for requests of statistical data
+#
+#
+#
 
 import auditor
+import math
 
 class Statistics(auditor.Auditor):
 	def _init_(self, time_length):
 		super()._init_(time_length)
-		
+
 		self.mean = 0.0
-	
+		self.log_mean = 0.0
+
 	def process(self):
+		self.log_mean = self.calcLogMean()
 		self.mean = self.calcMean()
-		'''
-		if self.mean > 500:
-			with open('logs.txt', 'a') as log:
-				log.write('\n\nError: statistical calculation error\n'
-							+ 'mean: ' + str(self.mean))
-				for point in self.data:
-					log.write(str(point) + '\n')
-			print(self.data)
-		print(self.mean)
-		'''
-		
+
+
 	def calcMean(self):
 		mean = 0.0
 		for point in self.data:
 			mean = mean +  (float(point['percentage']) * float(point['price']))
 		return mean
-		
+
+	def calcLogMean(self):
+		mean = 0.0
+
+		for point in self.data:
+			mean = mean +  (float(point['percentage']) * float(point['price']))
+
+		return 10*math.log(mean, 10)
--- teststrategy.py
+++ teststrategy.py
@@ -10,12 +10,13 @@ class TestStrat(strategy.Strategy):
 		#self.stats_long = statistics.Statistics(10*60)
 		#self.registerAuditor(self.stats_long)
 
-		self.stats_short = statistics.Statistics(60, 'BTC-USD')
+		self.stats_short = statistics.Statistics(60*60, 'BTC-USD')
 		self.registerAuditor(self.stats_short)
 
 	def strategy(self):
 		#print(self.stats_long.mean)
 		print(self.stats_short.mean)
+		print(self.stats_short.log_mean)
 
 		#if abs(self.stats_long.mean - self.stats_short.mean) < 10.00:
 		#	print('buy')
